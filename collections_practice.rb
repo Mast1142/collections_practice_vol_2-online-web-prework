7d4eb962061c2bc339edec5b16dc96c38086f5f1
Samuel W Tong <ssamt1142@gmail.com>
2019-03-04 02:33:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,27 @@
-# your code goes here
+def begins_with_r(array)
+  array.each do |word|
+    if word.start_with?("r") == true
+    else
+      return false
+    end
+  end
+  return true
+end
+
+def contain_a(array)
+  contain = []
+  array.each do |word|
+    if word.include?("a")
+      contain << word
+    end
+  end
+  contain
+end
+
+def first_wa(array)
+  array.each do |word|
+    if word.to_s.include?("wa") == true
+    return word
+    end
+  end
+end
